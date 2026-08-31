4d4843977cafdf7c8fa6363a446dbe328c07bb88
sunyong <gom1245@daum.net>
2026-08-31 15:31:14 +0900

    중간저장

---
+++ day07/script.js
@@ -0,0 +1,23 @@
+const movie = {
+  id: 550,
+  title: "Fight Club",
+  vote_average: 8.4,
+  vote_count: 0,
+  detail: {
+    runtime: 139,
+  },
+};
+
+const genres = ["Drama", "Thriller", "Crime"];
+
+// const { title, vote_average } = movie;
+// console.log(`영화 제목:${title}`);
+// console.log(`평점:${vote_average}`);
+
+// const { title: tt, vote_average: vag } = movie;
+
+// console.log(`영화 제목:${tt}`);
+// console.log(`평점:${vag}`);
+
+
+const
